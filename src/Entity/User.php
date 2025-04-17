7a9366246796380f40e93fb48101ca17bdc1deed
marwen24849 <marwenbelakhel695@gmail.com>
2025-04-17 12:52:13 +0200

    sideBAr

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -62,7 +62,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         return $this;
     }
 
-    public function getFirstName()
+    public function getfirst_name()
     {
         return $this->first_name;
     }
@@ -72,7 +72,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         $this->first_name = $value;
     }
 
-    public function getLastName()
+    public function getlast_name()
     {
         return $this->last_name;
     }
